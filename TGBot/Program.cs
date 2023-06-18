7baef2abbd8301f58b5dee8cc253d868863389f9
Bohdan <bohdanvoroniak455@gmail.com>
2023-06-18 16:38:46 +0300

    Add project files.

---
+++ TGBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Resources;
+using Telegram.Bot;
+using TGBot.Telegram;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<Bot>();
+    }).Build();
+host.Services.GetService<Bot>()!.Start();
+await host.RunAsync();
---
+++ TGBot/Telegram/Bot.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TGBot.Telegram
+{
+    public class Bot
+    {
+        private readonly ILogger<Bot> _logger;
+
+        public Bot(ILogger<Bot> logger)
+        {
+            _logger = logger;
+        }
+        public void StartReceivingUpdates()
+        {
+            var botClient = new TelegramBotClient(TelegramSettings.Token);
+
+            using CancellationTokenSource cts = new();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+            };
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                // Only process Message updates: https://core.telegram.org/bots/api#message
+                if (update.Message is not { } message)
+                    return;
+                // Only process text messages
+                if (message.Text is not { } messageText)
+                    return;
+                _logger.LogInformation(messageText);
+            }
+
+            Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                var ErrorMessage = exception switch
+                {
+                    ApiRequestException apiRequestException
+                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                    _ => exception.ToString()
+                };
+
+                Console.WriteLine(ErrorMessage);
+                return Task.CompletedTask;
+            }
+
+        }
+    }
+}
