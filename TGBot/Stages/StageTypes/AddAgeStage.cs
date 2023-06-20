474cd2508b19bf8aadd2d197e75f39f90622f109
Bohdan <bohdanvoroniak455@gmail.com>
2023-06-20 23:32:40 +0300

    Add age stage

--- TGBot/Stages/StageTypes/AddAgeStage.cs
+++ TGBot/Stages/StageTypes/AddAgeStage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,29 @@ namespace TGBot.Stages.StageTypes
 {
     public class AddAgeStage : IStage
     {
+        private readonly ITelegramUserService _telegramUserService;
+
+        public AddAgeStage(ITelegramUserService telegramUserService)
+        {
+            _telegramUserService = telegramUserService;
+        }
         public async Task HandleAsync(ITelegramBotClient botClient, Update update)
         {
-            var chatId = update.Message.Chat.Id;
-            await botClient.SendTextMessageAsync(chatId, $"I'm in addAge stage with message {update.Message.Text}");
+            var chatId = update.Message!.Chat.Id;
+            var textMessage = update.Message.Text;
+            int age;
+            if (textMessage is null)
+            {
+                await botClient.SendTextMessageAsync(chatId, "Неправильний тип повідомлення");
+                return;
+            }
+            if(!int.TryParse(textMessage,out age) || !(1 <= age && age <= 140))
+            {
+                await botClient.SendTextMessageAsync(chatId, "Неправильний формат числа(має бути число в межах (1-140)");
+                return;
+            }
+            await _telegramUserService.AddAgeAsync(chatId, age);
+            await botClient.SendTextMessageAsync(chatId, $"Тепер надішліть фотографію на оцінку");
         }
     }
 }
